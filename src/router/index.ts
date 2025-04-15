e4263e48a2049d945f1151f703967638a6eb0dfc
jjritsure <jose.julairitsure@cedsif.gov.mz>
2025-04-15 12:46:46 +0200

    Soliving images rendering issues

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 
 import PersonalInfo from "../pages/PersonalInfo.vue";
 import Education from "../pages/Education.vue";
@@ -40,7 +40,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 });
 
